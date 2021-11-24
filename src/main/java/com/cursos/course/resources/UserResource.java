de3085e721bb8d6f335d086306dd3a3c59236c8e
Tatiana Almeida TAB. de Brito <tatiana.brito@sspds.ce.gov.br>
2021-11-24 11:38:41 -0300

    User entity and resource

---
+++ src/main/java/com/cursos/course/resources/UserResource.java
@@ -0,0 +1,17 @@
+package com.cursos.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cursos.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}

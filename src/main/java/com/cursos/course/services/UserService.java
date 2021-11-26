16b326cbc00d13dc9d9625fa97e97acfec182092
Tatiana Almeida TAB. de Brito <tatiana.brito@sspds.ce.gov.br>
2021-11-26 17:01:26 -0300

    Exception handling - delete

--- src/main/java/com/cursos/course/services/UserService.java
+++ src/main/java/com/cursos/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.cursos.course.entities.User;
 import com.cursos.course.repositories.UserRepository;
+import com.cursos.course.services.exceptions.DatabaseException;
 import com.cursos.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	@SuppressWarnings("deprecation")
